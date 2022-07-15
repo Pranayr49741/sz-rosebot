24e265067060d1b0c254da70f528b117e211de2a
Pranayr49741 <96970138+Pranayr49741@users.noreply.github.com>
2022-07-15 22:14:01 +0530

    Update __main__.py

--- Rose/__main__.py
+++ Rose/__main__.py
@@ -22,7 +22,7 @@ import random
 import json
 import re
 
-from pyrogram import Client, Filters
+from pyrogram import Client, filters
 from pyrogram import InlineKeyboardMarkup, InlineKeyboardButton
 
 import functions as func
