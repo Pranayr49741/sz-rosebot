03032786603ccbee96bc80722dd79c9acaa171a8
Pranayr49741 <96970138+Pranayr49741@users.noreply.github.com>
2022-07-17 15:12:18 +0530

    Update __main__.py

--- Rose/__main__.py
+++ Rose/__main__.py
@@ -202,7 +202,7 @@ def button(client: app, callback_query: CallbackQuery):
         )
     
 
-@app.on_callback_query(filters.regex("types_back"))
+@app.on_callback_query(filters.regex("types_back in query.data"))
 def button2(client: app, callback_query: CallbackQuery):
     q_data = callback_query.data
     query_data = q_data.split('_')[1]
